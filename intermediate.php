ba7e58ea5a75d8927336119ea767519ae7e11ecd
nabila <nabila3905@student.unri.ac.id>
2024-09-25 11:52:56 +0700

    menambahkan paragraf

---
+++ intermediate.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar lagi</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, maxime alias! Ducimus ullam rerum necessitatibus placeat alias repudiandae eos non unde omnis ut quis sapiente accusantium amet, suscipit nostrum voluptatibus!</p>
+</body>
+</html>
